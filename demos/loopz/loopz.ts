db0d92f84210e00a6e1038107e4191a5ca2ed5d4
Adam Roberts <adarob@google.com>
2017-11-20 16:18:51 -0800

    Update loopz.ts

--- demos/loopz/loopz.ts
+++ demos/loopz/loopz.ts
@@ -17,7 +17,6 @@ limitations under the License.
 import { Array1D, Array2D, Array3D, CheckpointLoader, NDArray, NDArrayMathGPU, Scalar } from '../deeplearn';
 import { LSTMCell } from '../../src/math/math'
 import * as demo_util from '../util';
-// import { error } from 'shelljs';
 
 const DECODER_CELL_FORMAT = "decoder/multi_rnn_cell/cell_%d/lstm_cell/"
 
@@ -29,7 +28,6 @@ if (!DEBUG) {
   console.log = () => { };
 }
 const forgetBias = Scalar.new(1.0);
-//const presigDivisor = Scalar.new(2.0);
 let math: NDArrayMathGPU;
 
 class LayerVars {
@@ -84,12 +82,6 @@ class Encoder {
     const bwState = this.runLstm(sequence, this.lstmBwVars, true);
     const finalState = math.concat2D(fwState[1], bwState[1], 1)
     const mu = dense(this.muVars, finalState);
-    //const presig = dense(this.presigVars, finalState);
-    //const sigma = math.exp(math.arrayDividedByScalar(presig, presigDivisor));
-    /*
-    const z = math.addStrict(
-      mu,
-      math.multiplyStrict(sigma, math.track(Array2D.randNormal(sigma.shape)))) as Array2D;*/
     return mu;
   }
 }
@@ -222,19 +214,12 @@ class LoopZ {
       })
   }
 
-  //optomized encoding by Nikhil and Daniel
-  async encodeAndDecodeGPU(noteSequences: number[][], numSteps: number) {
+  // optimized encoding by Nikhil and Daniel
+  async interpolate(noteSequences: number[][], numSteps: number) {
     if (noteSequences.length != 2 && noteSequences.length != 4) {
       throw new Error('invalid number of note sequences. Requires length 2, or 4');
     }
     
-    // Shift noteSequences by 1
-    for (let i = 0; i < noteSequences[0].length; i++) {
-      for (let j = 0; j < noteSequences.length; j++) {
-        noteSequences[j][i]++;
-      }
-    }
-
     const z = math.scope((keep, track) => {
       // One-hot encode
       const startMelody = track(Array1D.new(noteSequences[0]));
@@ -293,187 +278,6 @@ class LoopZ {
         this.decoder.decode(interpolatedZs, noteSequences[0].length), this.offset) as Array2D;
     });
   }
-
-  //takes a 4X32 2D array of onehot encoded notes
-  async encodeAndDecode(noteSequences: number[][], numColumns: number, numRows: number) {
-
-    //create a 3d array where each the note being on / off is represented with a 1 or 0
-    const noteSequencesLength = noteSequences[0].length;
-    let inputs = Array3D.zeros([noteSequences.length, noteSequencesLength, this.decoder.outputDims]);
-    for (let i = 0; i < noteSequences.length; ++i) {
-      for (let j = 0; j < inputs.shape[1]; ++j) {
-        inputs.set(1, i, j, noteSequences[i][j] + 1);
-      }
-    }
-
-    var start = Date.now();
-    let allLabels = math.scope((keep, track) => {
-
-      //encode the inputs into the latent spcae. returns a 4 X 512 array.
-      const z = this.encoder.encode(inputs);
-
-      const z_vals = this.convertArray2DToJSArray2D(z); //4(tracks) X 512(latent space)
-
-      let labels: Array2D = track(Array2D.zeros([numRows * numColumns, z_vals[0].length]));
-      // const size = 10.0;
-      let index = 0;
-      for (let i = 0; i < numRows; i++) {
-        for (var j = 0; j < numColumns; j++) {
-          const latentSpaceX = j / (numColumns - 1);
-          const latentSpaceY = i / (numRows - 1);
-          // console.log(latentSpaceX + ' ' + latentSpaceY);
-          const latentSpaceZ = (this.convertPointToZ(z_vals, latentSpaceX, latentSpaceY));
-          // console.log(latentSpaceZ);
-          for (var k = 0; k < latentSpaceZ.length; k++) {
-            labels.set(latentSpaceZ[k], index, k);
-          }
-          index += 1;
-        }
-      }
-      // labels
-      if (DEBUG) {
-        console.log('labels.getValues()');
-        console.log(this.convertArray2DToJSArray2D(labels));
-      }
-
-      // this.printArray2D(labels);
-      return this.decoder.decode(labels, noteSequencesLength);
-    });
-
-    console.log((Date.now() - start) / 1000.);
-
-    //convert
-    return await allLabels.data().then(data => { //run asycn to free up the UI thread
-      let labels: number[][][] = [];
-      let tempNotes: number[] = [];
-      let count = 0;
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        tempNotes.push(data[i] - 1);
-        count++;
-        if (count == noteSequencesLength) {
-          if (this.mode == LoopZModel.drums) {
-            console.log(tempNotes);
-            console.log(this.getEncodedLabels(9, tempNotes));
-            labels.push(this.getEncodedLabels(9, tempNotes));//currently testing
-          } else {
-            labels.push(this.getEncodedLabels(10, tempNotes));
-          }
-          tempNotes = [];
-          count = 0;
-        }
-      }
-      return labels;
-    });
-  }
-
-  //expands onehot encoded notes to a matrix of on/off notes
-  //notes: size of the
-  //Reverse label
-  getEncodedLabels(columnHeight: number, notes: number[]) {
-    const noteMatrix = Array2D.zeros([columnHeight, notes.length]);
-    for (var i = 0; i < notes.length; i++) {
-      // const triggeredNotes = this.reverseOneHotEncodeHelper(notes[i], []);
-      // console.log(triggeredNotes);
-      const triggeredNotes = this.getLabelsHelper(notes[i], columnHeight);
-      triggeredNotes.forEach((note) => {
-        noteMatrix.set(1, note, i);
-      });
-    }
-    return this.convertArray2DToJSArray2D(noteMatrix);
-  }
-
-  getLabelsHelper(val: number, columnHeight: number): number[] {
-    let output: number[] = [];
-    for (var i = 0; i < columnHeight; i++) {
-      if (((val >> i) & 1) == 1) {
-        output.push(i);
-      }
-    }
-    return output;
-  }
-
-  /*
-  getLabelsHelper(val: number, encoding: number[]): number[] {
-    if (val < 0.0) return encoding;
-    const base = Math.log2(val);
-    // console.log(val + ' ' + base + ' ' + base%1)
-    if (base == 0) {
-      encoding.push(base);
-      return encoding;
-    } else if (base % 1 == 0) { //no desimal
-      encoding.push(base);
-      val -= val;
-    } else {
-      val -= 1;
-    }
-    return this.reverseOneHotEncodeHelper(val, encoding);
-  }
-  */
-
-  convertArray2DToJSArray2D(arr: Array2D) {
-    const finalArr: number[][] = []; //4(tracks) X 512(latent space)
-    let tempArr: number[];
-    for (var i = 0; i < arr.shape[0]; i++) {
-      tempArr = [arr.shape[1]];
-      for (var j = 0; j < arr.shape[1]; j++) {
-        tempArr[j] = arr.get(i, j);
-      }
-      finalArr.push(tempArr);
-    }
-    return finalArr;
-  }
-
-  convertArray2DToJSArray1D(arr: Array2D) {
-    const [x, y] = arr.shape;
-    const simple2dArray = [];
-    for (let i = 0; i < x; i++) {
-      const simpleArray = [];
-      for (let j = 0; j < y; j++) {
-        const point = arr.get(i, j);
-        simpleArray.push(point);
-      }
-      simple2dArray.push(simpleArray);
-    }
-    return simple2dArray;
-  }
-
-  // Takes 2 vecs and finds a point in between them at a given midPoint
-  // midPoint is 0-1, 0 being xVec and 1 being yVec
-  linearInterp(xVec: number[], yVec: number[], midPoint: number) {
-    const midVec = [];
-    for (var i = 0; i < xVec.length; i++) {
-      const distance = yVec[i] - xVec[i];
-      const midVal = xVec[i] + distance * midPoint;
-      midVec.push(midVal);
-    }
-    return midVec;
-  }
-
-  biLinearInterp(q11: number[], q12: number[], q21: number[],
-    q22: number[], x: number, y: number) {
-    // Linear interp along one direction
-    const interpX1 = this.linearInterp(q11, q21, x);
-    const interpX2 = this.linearInterp(q12, q22, x);
-
-    // Linear interp along the other direction using the previous interps
-    const interpY = this.linearInterp(interpX1, interpX2, y);
-    return interpY;
-  }
-
-  convertPointToZ(z_bounds: number[][], x: number, y: number) {
-    if (z_bounds.length == 0 || z_bounds.length == 1) {
-      return null;
-    } else if (z_bounds.length == 2) {
-      return this.linearInterp(z_bounds[0], z_bounds[1], x);
-    } else if (z_bounds.length == 3) {
-      return null; //TODO Add tirangular interpolation
-    } else if (z_bounds.length == 4) {
-      return this.biLinearInterp(z_bounds[0], z_bounds[1], z_bounds[2], z_bounds[3], x, y);
-    } else {
-      return null;
-    }
-  }
 }
 
 //this will be handled by the prototype
@@ -494,21 +298,12 @@ new LoopZ('./drums', async (err: Error, loopZ: LoopZ) => {
 
   var start = Date.now();
 
-  start = Date.now();
-  loopZ.encodeAndDecode(drums, 11, 11).then(data => {
-    console.log(data);
-    // data.forEach((noteSeq) => { }
-  });
-  console.log('unoptimized : ' + (Date.now() - start) / 1000.);
-
-  start = Date.now();
-  let data = await loopZ.encodeAndDecodeGPU(teaPots, 121);
+  let data = await loopZ.encodeAndDecodeGPU(teaPots, 90);
   console.log('optimized: ' + (Date.now() - start) / 1000.);
 
   for (let i = 0; i < data.shape[0]; i++) {
     const r = math.slice2D(data, [i, 0], [1, data.shape[1]]);
     await r.data()
-    // console.log(await r.data());
   }
   console.log('gpu format data: ' + (Date.now() - start) / 1000.);
 });
